b1de19e20ecdc652cc7450b14a6a969cd858ef38
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2017-08-20 12:02:59 +0200

    Fib recursive and fib iterative

---
+++ fib.py
@@ -0,0 +1,57 @@
+def fib_recurse(n):
+	"""
+	n = Integer
+	Recursively calculate fibonacchi of n
+	"""
+	if (n <= 1):
+		return n
+	else:
+		return fib_recurse(n - 1) + fib_recurse(n - 2)
+
+def fib_table(n):
+	"""
+	n = Integer
+	Faster fibonacci of n
+	"""
+	table = [0]
+	first = 0
+	second = 1
+
+	for i in range(n):
+		table.append(second)
+		third = first + second
+
+		first = second
+		second = third
+		
+	return table[-1]
+
+import time
+
+def stressTest(n):
+	"""
+	Output of both above functions should be identical.
+	Difference is comuting times where fib_table should be faster.
+	"""
+	t1 = time.time()
+	slow_fib = fib_recurse(n)
+	t2 = time.time()
+	diff1 = t2 - t1
+	
+	t3 = time.time()
+	fast_fib = fib_table(n)
+	t4 = time.time()
+	diff2 = t4 - t3
+	
+	if slow_fib == fast_fib:
+		print("Fib of", str(n), "is equal by both functions:", fast_fib)
+	else:
+		print("Fib of", str(n), "is NOT equal by both functions.", "'slow_fib'=", slow_fib, "'fast_fib'=", fast_fib)
+	
+	if diff1 > diff2:
+		print("'fib_table' faster than 'fib_table'")
+	elif diff1 < diff2:
+		print("'fib_recurse' faster than 'fib_table'")
+	else:
+		print("both functions were computed in equal times")
+
